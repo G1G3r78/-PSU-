684b319cda238efc8c9fa8f22ab889a57fb602de
G1G3r78 <148001281+G1G3r78@users.noreply.github.com>
2026-03-21 23:35:54 +0500

    Add files via upload

--- srs.cpp
+++ srs.cpp
@@ -1,145 +1,154 @@
 #include <iostream>
 
+#include <queue>
+#include <stack>
+#include <list>
+
 using namespace std;
 
-struct och {
-    int d;
-    och *next;
-};
+//уже реализовано в библ. STL
+/*template <class T, class Container = deque<T> >
+class queue;*/
 
-struct STEK {
-    double d;
-    STEK *next;
-};
+/*template <class T, class Container = deque<T> >
+class stack;*/
 
-struct STEK2 {
-    double sum;
-    double mult;
-    double sr;
-};
-
-//процедура вывода очереди через пробел
-void outpQueue(och* nach) {
-    while (nach != NULL) {
-        cout << (*nach).d << " ";
-        nach = (*nach).next;
+void outpSP(list<int> SP) {
+    for (int numSP: SP) {
+        cout << numSP << " ";
     }
+    cout << endl;
 }
 
-//процедура вывода стека
-void outpStek(STEK* head) {
-    int elNum = 1;//счётчик элементов
-    while (head != NULL) {
-        STEK *yd = head;
-        cout << elNum << " элемент - " << yd->d << endl;
-        head = (*head).next;  // переход к следующему
-        delete yd;          // удаление текущего
-        elNum++;
+list<int> zapSP(list<int> SP, int n) {
+    cout << "Заполнение первой половины списка с начала:\n";
+    for (int i = 0; i < n/2; i++) {
+        SP.push_front(rand() % 100);
+        outpSP(SP);
     }
+    cout << "Заполнение другой половины списка с конца:\n";
+    for (int j = n/2; j < n; j++) {
+        SP.push_back(rand() % 100);
+        outpSP(SP);
+    }
+    return SP;
 }
 
 int main() {
 
     srand(time(NULL));
 
-    // ==========
-    // Задание 1
+    //задание 1
+    //Очередь
+    queue<int> q;
 
-    //инициализация очереди
-    och *nach;
-    och *end;
-    nach = NULL;
-    end = NULL;
+    //заполнение
+    q.push(10);
+    q.push(20);
+    q.push(30);
 
-    //заполнение очереди
-    for (int i = 1; i <= 10; i++) {
-        och *t;
-        t = new (och);
-        (*t).d = rand() % 100;
-        if (end == NULL) {
-            end = t;
-            nach = t;
-        } else {
-            (*end).next = t;
-            end = t;
-        }
-        cout << i << " элемент очереди = " << (*end).d << endl;
+    cout << "Очередь:\n";
+    cout << "Первый элемент: " << q.front() << endl;
+    cout << "Последний элемент: " << q.back() << endl;
+    cout << "Размерность: " << q.size() << endl;
+
+    // вывод и удаление
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
     }
+    //проверка пустоты
+    cout << (q.empty() ? "\nОчередь пуста" : "\nОчередь заполнена") << endl;
 
-    //вывод через пробел значений очереди
-    outpQueue(nach);
+    //Стек
+    stack<int> s;
 
-    //освобождение памяти
-    nach = NULL;
-    end = NULL;
-    delete nach;
-    delete end;
+    // заполнение
+    s.push(1);
+    s.push(2);
+    s.push(3);
 
-    // =================
+    cout << "\nСтек:\n";
+    cout << "Вершина стека: " << s.top() << endl;
+    cout << "Размер: " << s.size() << endl;
 
-    // =================
-    // 2 задание
-
-    //инициализация стека
-    STEK *head;
-    head = NULL;
-
-    int n;
-    double sum = 0.0, mult = 1.0, sr = 0.0;
-
-    cout << "\nВведите кол-о элементов стека: ";
-    cin >> n;
-
-    //заполнение стека
-    for (int i = 0; i < n; i++) {
-        STEK *p = new STEK;
-        (*p).d = (rand() % 100) * 0.1;
-        (*p).next = head;  // новый элемент указывает на текущую голову
-        head = p;        // новый элемент становится головой
-
-        cout << "Добавление элемента " << (*head).d << endl;
-        sum += (*p).d;
-        mult *= (*p).d;
+    //удаление
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
     }
-    sr = sum / n;
+    //проверка пустоты
+    cout << (s.empty() ? "\nСтек пуст" : "\nСтек заполнен") << endl;
 
-    cout << "Полный стек:\n";
-    outpStek(head);
+    //задание 2
+    int nSP, delNumSP, addNumSP, indSP, amountSP, numsIndSP;
+    list<int> SP;
+    cout << "Введите кол-о элементов list: ";
+    cin >> nSP;
 
-    //инициализация и заполнение STEK2
-    cout << "Сумма = " << sum << ", произв. = " << mult << ", ср. ариф. = " << sr << endl;
-    STEK2 *t;
-    t = new STEK2;
-    (*t).sum = sum;
-    (*t).mult = mult;
-    (*t).sr = sr;
+    cout << "Заполнение list:\n";
+    SP = zapSP(SP, nSP);
+    cout << "Вывод полного list:\n";
+    outpSP(SP);
+
+    cout << "Какой элемент удалить: ";
+    cin >> delNumSP;
+
+    list<int>::iterator p = SP.begin();
+    bool found = false;
+    while (p != SP.end()) {
+        if (*p == delNumSP) found = true;
+        p++;
+    }
+    if (found) SP.remove(delNumSP);
+        else cout << "Число не найдено\n";
+    cout << "Вывод list после удаления:\n";
+    if (SP.empty()) cout << "list пуст\n";
+     else outpSP(SP);
+
+    cout << "Какое число вставить: ";
+    cin >> addNumSP;
+    cout << "На какой индекс вставить: ";
+    cin >> indSP;
+
+    auto addP = SP.begin();
+    advance(addP, indSP);
+    SP.insert(addP, addNumSP);
+    cout << "list после добавления:\n";
+    outpSP(SP);
+
+    cout << "Сколько чисел вставить: ";
+    cin >> amountSP;
+    cout << "На какой индекс эти числа вставить: ";
+    cin >> numsIndSP;
+    addP = SP.begin();
+    advance(addP, numsIndSP);
+    for (int i = 0; i < amountSP; i++) {
+        int value = rand() % 100;
+        addP = SP.insert(addP, value);
+        advance(addP, 1);
+    }
+    cout << "list после добавления:\n";
+    outpSP(SP);
+
+    int rangeIndBeg, rangeIndEnd;
+    cout << "Укажите диапазон вставки элементов:\n";
+    cout << "Укажите индекс начала диапазона: ";
+    cin >> rangeIndBeg;
+    cout << "Укажите индекс конца диапазона: ";
+    cin >> rangeIndEnd;
+
+    auto startP = SP.begin(), endP = SP.end();
+    advance(startP, rangeIndBeg);
+    advance(endP, rangeIndEnd);
+
+    for (int i = rangeIndBeg; i <= rangeIndEnd; i++) {
+        int value = rand() % 100;
+        startP = SP.insert(startP, value);
+        advance(startP, 1);
+    }
+    cout << "list после вставки диапазона:\n";
+    outpSP(SP);
 
     return 0;
 }
-
-/*
-1 элемент очереди = 11
-2 элемент очереди = 6
-3 элемент очереди = 74
-4 элемент очереди = 23
-5 элемент очереди = 69
-6 элемент очереди = 38
-7 элемент очереди = 84
-8 элемент очереди = 69
-9 элемент очереди = 69
-10 элемент очереди = 56
-11 6 74 23 69 38 84 69 69 56
-Введите кол-о элементов стека: 5
-Добавление элемента 2.2
-Добавление элемента 4.3
-Добавление элемента 4.5
-Добавление элемента 4.5
-Добавление элемента 5.6
-Полный стек:
-1 элемент - 5.6
-2 элемент - 4.5
-3 элемент - 4.5
-4 элемент - 4.3
-5 элемент - 2.2
-Сумма = 21.1, произв. = 1072.76, ср. ариф. = 4.22
- */
